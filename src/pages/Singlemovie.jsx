1f7a9b534f4a43402a18838b4bd1a4f96d965c03
Shehuadekunmi <sirkumyemo@gmail.com>
2023-09-12 23:50:35 +0100

    finishing layout

--- src/pages/Singlemovie.jsx
+++ src/pages/Singlemovie.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import Card from 'react-bootstrap/Card';
+import tra from '../assets/tral.png'
+import '../style/Singlemovie.css'
 
 const Singlemovie = () => {
   return (
     <div>Singlemovie
       <Sidebar/>
+
+
+
+      <div className='watch d-flex justify-content-center   text-white'>
+          <h1>Watch Trailer</h1>
+        </div>
+      <section className='single my-3'>
+        <h1>hellooooooo</h1>
+
+      </section>
     </div>
   )
 }
